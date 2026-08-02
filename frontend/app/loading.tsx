ed5ea2c0f199943bac0fb87fe421d9da51a93e06
milon <milonmondolmd33@gmail.com>
2026-08-02 19:18:27 +0600

    update skeleton

--- frontend/app/loading.tsx
+++ frontend/app/loading.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Skeleton } from "@/components/ui/skeleton"
 
 const GlobalLoading = () => {
   return (
-    <div>
-      GlobalLoading
+    <div className="flex flex-col space-y-3 p-6 max-w-md mx-auto mt-10">
+      <Skeleton className="h-[125px] w-full rounded-xl" />
+      
+      {/* টেক্সট স্কিলটন */}
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
     </div>
   )
 }
